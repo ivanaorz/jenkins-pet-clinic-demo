83049220aad0c68cb6da0a4be33caefdc153249a
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-09-14 14:42:08 +0200

    adding bare project

---
+++ src/test/java/com/example/jenkinspetclinicdemo/JenkinsPetClinicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspetclinicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPetClinicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
