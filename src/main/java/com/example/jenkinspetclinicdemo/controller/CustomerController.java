a729ec991c6b7f91118b3d554f7e7915cd88e9c4
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-09-15 14:27:51 +0200

    testing saveCustomer method

---
+++ src/main/java/com/example/jenkinspetclinicdemo/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package com.example.jenkinspetclinicdemo.controller;
+
+public class CustomerController {
+
+    public String saveCustomer(String name) {
+        return name;
+    }
+}
---
+++ src/test/java/com/example/jenkinspetclinicdemo/controller/CustomerControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.jenkinspetclinicdemo.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+    @Test
+    void saveCustomer() {
+
+        String name = "Pet";
+
+        Assertions.assertEquals("Dog", name);
+    }
+
+}
